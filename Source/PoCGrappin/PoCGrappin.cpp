64d35b38590d3c9281a266658c5788364ef6c9fa
Manoël Nohra <manoel.nohra@gmail.com>
2022-02-12 14:55:13 -0500

    Recréation du projet pour pouvoir push sans problème sur git en espérant que cela fonctionne

---
+++ Source/PoCGrappin/PoCGrappin.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PoCGrappin.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PoCGrappin, "PoCGrappin" );
---
+++ Source/PoCGrappin/PoCGrappinGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PoCGrappinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POCGRAPPIN_API APoCGrappinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

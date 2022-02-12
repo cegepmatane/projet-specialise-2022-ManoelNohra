6dd85ac1011de2094afc9f2b494d4f17987847f5
Manoël Nohra <manoel.nohra@gmail.com>
2022-02-12 15:50:06 -0500

    J'ai suivi les tutoriels sur la documentation d'Unreal Engine pour m'initier à l'engin : https://docs.unrealengine.com/4.27/en-US/ProgrammingAndScripting/ProgrammingWithCPP/CPPProgrammingQuickStart/ https://docs.unrealengine.com/4.27/en-US/ProgrammingAndScripting/ProgrammingWithCPP/CPPTutorials/ https://docs.unrealengine.com/4.27/en-US/ProgrammingAndScripting/ProgrammingWithCPP/CPPTutorials/AutoCamera/ https://docs.unrealengine.com/4.27/en-US/ProgrammingAndScripting/ProgrammingWithCPP/CPPTutorials/PlayerCamera/ https://docs.unrealengine.com/4.27/en-US/ProgrammingAndScripting/ProgrammingWithCPP/CPPTutorials/PlayerInput/ #2 #8

---
+++ Source/PoCGrappin/MyPawn.cpp
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPawn.h"
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	speed = 100.0f;
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	// Create a dummy root component we can attach things to.
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	// Create a camera and a visible object
+	 OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("OurCamera"));
+	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
+	// Attach our camera and visible object to our root component. Offset and rotate the camera.
+	OurCamera->SetupAttachment(RootComponent);
+	OurCamera->SetRelativeLocation(FVector(0, 0.0f, 0.0f));
+	OurCamera->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
+	OurVisibleComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float CurrentScale = OurVisibleComponent->GetComponentScale().X;
+	if (bGrowing)
+	{
+		// Grow to double size over the course of one second
+		CurrentScale += DeltaTime;
+	}
+	else
+	{
+		// Shrink half as fast as we grow
+		CurrentScale -= (DeltaTime * 1.0f);
+	}
+	// Make sure we never drop below our starting size, or increase past double size.
+	CurrentScale = FMath::Clamp(CurrentScale, 1.0f, 5.0f);
+	OurVisibleComponent->SetWorldScale3D(FVector(CurrentScale));
+
+	if (!CurrentVelocity.IsZero())
+	{
+		FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+		SetActorLocation(NewLocation);
+	}
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Respond when our "Grow" key is pressed or released.
+	InputComponent->BindAction("Grow", IE_Pressed, this, &AMyPawn::StartGrowing);
+	InputComponent->BindAction("Grow", IE_Released, this, &AMyPawn::StopGrowing);
+	InputComponent->BindAction("Sprint", IE_Pressed, this, &AMyPawn::StartSprinting);
+	InputComponent->BindAction("Sprint", IE_Released, this, &AMyPawn::StopSprinting);
+
+	// Respond every frame to the values of our two movement axes, "MoveX" and "MoveY".
+	InputComponent->BindAxis("MoveX", this, &AMyPawn::Move_XAxis);
+	InputComponent->BindAxis("MoveY", this, &AMyPawn::Move_YAxis);
+
+}
+
+void AMyPawn::Move_XAxis(float AxisValue)
+{
+	// Move at 100 units per second forward or backward
+	CurrentVelocity.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * speed;
+
+}
+
+void AMyPawn::Move_YAxis(float AxisValue)
+{
+	// Move at 100 units per second right or left
+	CurrentVelocity.Y = FMath::Clamp(AxisValue, -1.0f, 1.0f) * speed;
+}
+
+void AMyPawn::StartGrowing()
+{
+	bGrowing = true;
+}
+
+void AMyPawn::StopGrowing()
+{
+	bGrowing = false;
+}
+
+void AMyPawn::StartSprinting()
+{
+	speed = 500.0f;
+}
+void AMyPawn::StopSprinting()
+{
+	speed = 100.0f;
+}
+
